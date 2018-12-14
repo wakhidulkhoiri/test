b2d2581b2130dca4f4851ee7473fce07da6a30cf
wakhidulkhoiri <42726508+wakhidulkhoiri@users.noreply.github.com>
2018-12-14 22:49:30 +0700

    Update user.php

--- user.php
+++ user.php
@@ -8,6 +8,6 @@ $msgwe = "[!] info withdraw work with delay 10 sec";
 $msgveeu = "[!] enjoy your life";
 $data= [
 'unbk Ruang 2','Faqih Khoiril Anam','wakhidul khoiri','Ashter Konoha','Dwi Putri Handayani','Nawirul Fadly',
- 'Faqih Khoiril Anam','Wakhidul Khoiri','elvanza nabila','Silvia Diaz389','User_1527989487501','ZELL• MLBB','Saiful arif in channel','Media Onlineku'       ];
+ 'Faqih Khoiril Anam','Wakhidul Khoiri','elvanza nabila','Silvia Diaz389','User_1527989487501','ZELL• MLBB','Saiful arif in channel','Media Onlineku','Retno Keyza'       ];
 $ydnews = ['tes',];
 $toto = ['tes',];
