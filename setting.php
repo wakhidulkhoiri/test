928b20e5c464b2168e356699127710fc829ba1d2
wakhidulkhoiri <42726508+wakhidulkhoiri@users.noreply.github.com>
2019-09-28 01:11:32 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,22 +1,18 @@
 <?php
 /* setting bot */
-$ads=$ijo."    [ OPREK BARENG ]".$t.$red."\t >> Visit Blogger << ".$t.$pth."[ ".$ijo."https://oprek-bareng.blogspot.com".$pth." ]=[".$kn." Oprek Bareng ".$pth."]=[ ".$tr."Iwan Ashter ]".$t;
+$ads=$ijo."    [ Big Token ]".$t.$red."\t >> Kok Di Gawe Mumet << ".$t.$pth."[ ".$ijo."Jomblo Pasti Kesepian".$pth." ]=[".$kn." Oprek Bareng ".$pth."]=[ ".$tr."Iwan Ashter ]".$t;
 
 $donasi=false;
 
 $warn=$red."[!] Perhatian!!!
 ".$kn."[-] Ini Adalah Program Ilegal.....!!!!
 ".$kn."[-] Oppit Bagi - Bagi".$t;
-
-$key="adidoank";
-$link_yogo="https://youtu.be/hLkAqZM3QHc";
-
 $big=$br."
 ──── █▀▄ ▀ ▄▀▀─────".$tr." ▀█▀ ▄▀▄ █─▄▀ █▀▀ █▄─█────".$br."
 ──── █▀█ █ █─▀▌────".$tr." ─█─ █─█ █▀▄─ █▀▀ █─▀█────".$br."
 ──── ▀▀─ ▀ ▀▀▀─────".$tr." ─▀─ ─▀─ ▀─▀▀ ▀▀▀ ▀──▀────".$pth."
-=| creator : ".$ijo."Soppho Wae ".$pth."|==| versi :".$tr." [ 1.0 ]".$pth." |=".$t;
-$msg_big="[•]=[ Penak men Uripmu ]";
+=| Script : ".$ijo."Big Token ".$pth."|==| versi :".$tr." [ 2.0 ]".$pth." |=".$t;
+$msg_big="[•]=[ Penak Temen Uripmu ]";
 $stat_big=true;
 
 ?>
