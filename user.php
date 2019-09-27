223c7eda09e847383f5915ca1cb242c7ced50a30
wakhidulkhoiri <42726508+wakhidulkhoiri@users.noreply.github.com>
2019-09-28 00:52:45 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,16 +1,13 @@
 <?php
-echo $red."[!] warning !!!".$t;
-echo $kuning."[-] ini adalah program ilegal".$t;
-echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
-$msgtoto = "[!] info toto news berganti menjadi TTNews".$t."[-] gunakan akun toto news untuk login di TTNews".$t."[-] skrip tetap work dengan app TTNews".$t."[-] cara tranfer akun login di TTNews pilih old user masukan kode invite akun toto news tranfer akun selesai";
-$msgyd = "[!] enjoy your life";
-$msgwe = "[!] info withdraw work with delay 10 sec";
-$msgveeu = "[!] enjoy your life";
-$data= [
-'unbk Ruang 2','Faqih Khoiril Anam','wakhidul khoiri','Ashter Konoha','Dwi Putri Handayani','Nawirul Fadly',
- 'Faqih Khoiril Anam','Wakhidul Khoiri','elvanza nabila','Silvia Diaz389','User_1527989487501','ZELL• MLBB',
- 'Saiful arif in channel','Media Onlineku','Retno Keyza','Pemuda Gang Tengah','gang tengah','nawirul fadly','wahyu ashter'      ];
-$ydnews = ['tes',];
-$toto = ['unbk Ruang 2','Faqih Khoiril Anam','wakhidul khoiri','Ashter Konoha','Dwi Putri Handayani','Nawirul Fadly',
- 'Faqih Khoiril Anam','Wakhidul Khoiri','elvanza nabila','Silvia Diaz389','User_1527989487501','ZELL• MLBB',
- 'Saiful arif in channel','Media Onlineku','Retno Keyza','Pemuda Gang Tengah','gang tengah','nawirul fadly','wahyu ashter'];
+$stat_sseth=true;
+$msgsseth= "[---] enjoy your life";
+$stat_cp = true;
+$msgcp = "[--] enjoy your life";
+$figlet= $ijo."
+───────────────────────────────────────────
+── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$kuning." [`  ||    _    _.       
+── █─ █▀█ █─█ █ █─▀█ █─▀▌".$kuning." | L||| \/(/_|`_\|".$ijo."
+── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
+  $putih."creator:".$ijo." adidoank   ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
+  $putih."chanel :".$ijo." adi bordir ".$putih."[--] chanel:".$ijo." sungging  ".$t.
+  $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo."  otr3o8   ".$t;
