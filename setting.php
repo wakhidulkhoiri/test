e11d66e44d88616e2d77cca2b461e2c37d2a98fd
wakhidulkhoiri <42726508+wakhidulkhoiri@users.noreply.github.com>
2019-09-28 12:34:44 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,13 +1,19 @@
 <?php
-$ads=$t.$red."\t >> Untuk Para Jomblo Enggak Usah Nikah << ".$t.$pth."".$ijo."Ada RUU KUHP Baru Perkosa Istri".$pth."".$kn." Denda 10 M".$pth."".$tr." Sabar Yaa Budjank....".$t;
-$warn=$red."[!] Perhatian!!!
+$ads=$t.$red."\t >> Untuk Para Jomblo Enggak Usah Nikah << ".$t.$pth."".$ijo."Ada RUU KUHP Baru Perkosa Istri".$pth."".$kn." Denda 10M".$pth."".$tr." Sabar Yaa Budjank....".$t;
+$warn=$red."||=•••> Perhatian.....!!!
 ".$kn."||=•••> Ini Adalah Program Ilegal.....!!!!
 ".$kn."||=•••> No Baned No Asyik.....!!!!
 ".$kn."||=•••> Oppit Bagi - Bagi.....!!!!".$t;
 $big=$br."
-──── █▀▄ ▀ ▄▀▀─────".$tr." ▀█▀ ▄▀▄ █─▄▀ █▀▀ █▄─█────".$br."
-──── █▀█ █ █─▀▌────".$tr." ─█─ █─█ █▀▄─ █▀▀ █─▀█────".$br."
-──── ▀▀─ ▀ ▀▀▀─────".$tr." ─▀─ ─▀─ ▀─▀▀ ▀▀▀ ▀──▀────".$pth."
+
+        (                             )        )            )    
+   (    )\ )   (           *   )   ( /(     ( /(         ( /(    
+ ( )\  (()/(   )\ )      ` )  /(   )\())    )\())  (     )\())   
+ )((_)  /(_)) (()/(       ( )(_)) ((_)\   |((_)\   )\   ((_)\    
+((_)_  (_))    /(_))_    (_(_())    ((_)  |_ ((_) ((_)   _((_)   
+ | _ ) |_ _|  (_)) __|   |_   _|   / _ \  | |/ /  | __| | \| |   
+ | _ \  | |     | (_ |     | |    | (_) |   ' <   | _|  | .` |   
+ |___/ |___|     \___|     |_|     \___/   _|\_\  |___| |_|\_| .$pth."
 ||=•••> Script : ".$ijo."Big Token ".$pth."||=•••> Tahun :".$tr."2019".$pth."".$t;
 $msg_big="||=•••> = Please Jangan Maruk :) ";
 $stat_big=true;
