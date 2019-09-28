ab113a1ec6c2138ae338b8c05ff70cf0b11d4d25
wakhidulkhoiri <42726508+wakhidulkhoiri@users.noreply.github.com>
2019-09-28 10:45:33 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -14,52 +14,5 @@ $big=$br."
 $msg_big="[•]=[ Penak Temen Uripmu ]";
 $stat_big=true;
 
-$biru="\033[1;34m";
-$turkis="\033[1;36m";
-$ijo="\033[92m";
-$putih="\033[1;37m";
-$pink="\033[1;35m";
-$red="\033[1;31m";
-$kuning="\033[1;33m";
-$n = "\n";
-
-echo $red.   " _____
-< Pencari Receh >
- ---------------
- ``                                                                    ..".$n.$n.$putih;
-echo $biru.   "       			  Scripts Protection".$n.$n.$putih;
-sleep(2);
-echo          "  		     Masukan password untuk login".$n;
-
-echo $putih." 	          Untuk password silakan menghubungi".$n.$ijo;
-sleep(2);
-echo "               	   https://oprek-bareng.blogspot.com".$n.$n;
-sleep(2);
-echo $putih." Masukan password".$n;
-$pass=file_get_contents('https://wakhidulkhoiri.web.id/pass.txt');
-echo $pink." Input pass$red :$turkis ";
-$valid=rtrim(fgets(STDIN),"\n");
-sleep(3);
-echo $biru." Cek Kebenaran Password".$n;
-switch($valid){
-case null:
-echo $red." Password Wajib Di Isi ......\n";
-break;
-case $pass:
-echo $putih." Sabar Membuka Akses Script ==> ";
-sleep(3);
-echo" 5";
-sleep(3);
-echo "   4";
-sleep(3);
-echo"    3";
-sleep(2);
-echo"    2";
-sleep(1);
-echo ".\n";
-@system("clear");
-sleep(1);
-@system("clear");
-
 
 ?>
