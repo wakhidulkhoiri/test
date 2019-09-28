8b24d362e45335eb6c8cf99a94c2149d43ce2af2
wakhidulkhoiri <42726508+wakhidulkhoiri@users.noreply.github.com>
2019-09-28 10:58:55 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,17 +1,15 @@
 <?php
 $ads=$t.$red."\t >> Untuk Para Jomblo Enggak Usah Nikah << ".$t.$pth."[ ".$ijo."Ada RUU KUHP Baru Perkosa Istri".$pth." ]=[".$kn."Denda 10 M".$pth."]=[ ".$tr."Colay Aja..!!!]".$t;
-
-$donasi=false;
-
 $warn=$red."[!] Perhatian!!!
 ".$kn."[-] Ini Adalah Program Ilegal.....!!!!
-".$kn."[-] Oppit Bagi - Bagi".$t;
+".$kn."[-] No Baned No Asyik.....!!!!
+".$kn."[-] Oppit Bagi - Bagi.....!!!!".$t;
 $big=$br."
 ──── █▀▄ ▀ ▄▀▀─────".$tr." ▀█▀ ▄▀▄ █─▄▀ █▀▀ █▄─█────".$br."
 ──── █▀█ █ █─▀▌────".$tr." ─█─ █─█ █▀▄─ █▀▀ █─▀█────".$br."
 ──── ▀▀─ ▀ ▀▀▀─────".$tr." ─▀─ ─▀─ ▀─▀▀ ▀▀▀ ▀──▀────".$pth."
 =| Script : ".$ijo."Big Token ".$pth."|==| Tahun :".$tr." [ 2019 ]".$pth." |=".$t;
-$msg_big="[•]=[ Penak Temen Uripmu ]";
+$msg_big="[•]=[ Jangan Maruk :) ]";
 $stat_big=true;
 
 
