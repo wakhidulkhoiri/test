dab18f9870b14243def51ba90670111bcf8bf085
wakhidulkhoiri <42726508+wakhidulkhoiri@users.noreply.github.com>
2019-09-28 12:17:58 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,15 +1,15 @@
 <?php
 $ads=$t.$red."\t >> Untuk Para Jomblo Enggak Usah Nikah << ".$t.$pth."".$ijo."Ada RUU KUHP Baru Perkosa Istri".$pth."".$kn." Denda 10 M".$pth."".$tr." Sabar Yaa Budjank....".$t;
 $warn=$red."[!] Perhatian!!!
-".$kn."|=•••> Ini Adalah Program Ilegal.....!!!!
-".$kn."|=••> No Baned No Asyik.....!!!!
-".$kn."|=•> Oppit Bagi - Bagi.....!!!!".$t;
+".$kn."||=•••> Ini Adalah Program Ilegal.....!!!!
+".$kn."||=•••> No Baned No Asyik.....!!!!
+".$kn."||=•••> Oppit Bagi - Bagi.....!!!!".$t;
 $big=$br."
 ──── █▀▄ ▀ ▄▀▀─────".$tr." ▀█▀ ▄▀▄ █─▄▀ █▀▀ █▄─█────".$br."
 ──── █▀█ █ █─▀▌────".$tr." ─█─ █─█ █▀▄─ █▀▀ █─▀█────".$br."
 ──── ▀▀─ ▀ ▀▀▀─────".$tr." ─▀─ ─▀─ ▀─▀▀ ▀▀▀ ▀──▀────".$pth."
-Script : ".$ijo."Big Token ".$pth."<===>Tahun :".$tr."2019".$pth."".$t;
-$msg_big="|=•••> = Jangan Maruk :) ";
+||=•••> Script : ".$ijo."Big Token ".$pth."||=•••> Tahun :".$tr."2019".$pth."".$t;
+$msg_big="||=•••> = Please Jangan Maruk :) ";
 $stat_big=true;
 
 
