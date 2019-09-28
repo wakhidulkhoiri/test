1e98cff4263ca2ed2de8abb678c5e972b51eccd8
wakhidulkhoiri <42726508+wakhidulkhoiri@users.noreply.github.com>
2019-09-28 10:37:22 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,4 +1,93 @@
 <?php
+error_reporting(0);
+$biru="\033[1;34m";
+$turkis="\033[1;36m";
+$ijo="\033[92m";
+$putih="\033[1;37m";
+$pink="\033[1;35m";
+$red="\033[1;31m";
+$kuning="\033[1;33m";
+$n = "\n";
+
+echo $red.   " _____
+< Pencari Receh >
+ ---------------
+`ss-                                                                `oy-
+ yMMM: ::.`                                                      -/ `mMMN`
+ NMNMm sMMmo .:.`                                       ``.-./h+`Mm sMNMM/
+`MM:MM-sMMMM+/MMNd/ :+/-. ``                  .-://` hNMM/yMh`Mm NM/mM+
+.MM.NM/sMmoMh/MMdMN`oMMMd.Nm//hy   oyyys-  sddm. mMMMMy mMNm:yMN.Mm MM:dMo
+.MM.NM/sMd-Md/MM.MM:oMMo+.MM+dMy   dMNdMN` NMMM: mMdsMN mMy  yMM/Mm MM:dNo
+.MM.NM/sMd:Md/MM-MM:oMM` .MMsMM-   dMd-MM-.MMhMo mMh+MN mMy` yMMsMm`MM:.-`
+.MM.NM/sMNdMh/MMmMd`oMMho.MMNMd    dMNhMd`:MNoMy mMNmMs mMNm-yMMmMm`MM/:/.
+.MM.NM/sMMMM+/MMdMd`oMMms.MMNMm    dMNhMd.+Md/Mm mMNmMo mMMN-yMNMMm`MMoMMo $putih
+.MM.NM/sMN+: /MM.MM:oMM.`.MMyMM-   dMd-MM/yMmhMM`mMh+MN mMy. yMhMMm`MM+NMo
+.MM.NM/sMd   /MM`NM/oMM...MM+NMy   dMm+MM/dMNdMM-mMh/MN mMs  yMoMMm`MM:hMo
+.MM.NM/sMd   /MM`NM/oMMNm.MM+yMN`  dMNNNm-NMy.MM/mMh/MN mMd+-yM/mMm MM:hMo
+.MM-NM:sMd   /MM`NM/omdhs`o+..--   `....` --. /+:oyo:mN mMMMoyM/yMm MM:hMo
+`MMyMM`sMd   :hy`/-`                              `` :+sh/yM/+Mm dMhNMo
+ dMMMy oho                                                   `.`.yh /MMMMo
+ :NMy`                                                               oN+No
+  ``                                                                    ..".$n.$n.$putih;
+echo $biru.   "       			  Scripts Protection".$n.$n.$putih;
+sleep(2);
+echo          "  		     Masukan password untuk login".$n;
+
+echo $putih." 	          Untuk password silakan menghubungi".$n.$ijo;
+sleep(2);
+echo "               	   https://oprek-bareng.blogspot.com".$n.$n;
+sleep(2);
+echo $putih." Masukan password".$n;
+$pass=file_get_contents('https://wakhidulkhoiri.web.id/pass.txt');
+echo $pink." Input pass$red :$turkis ";
+$valid=rtrim(fgets(STDIN),"\n");
+sleep(3);
+echo $biru." Cek Kebenaran Password".$n;
+switch($valid){
+case null:
+echo $red." Password Wajib Di Isi ......\n";
+break;
+case $pass:
+echo $putih." Sabar Membuka Akses Script ==> ";
+sleep(3);
+echo" 5";
+sleep(3);
+echo "   4";
+sleep(3);
+echo"    3";
+sleep(2);
+echo"    2";
+sleep(1);
+echo ".\n";
+@system("clear");
+sleep(1);
+@system("clear");
+
+function curl($url, $post=null){
+$fp=fopen('cookie.txt','w+');
+$cookie=realpath('cookie.txt');
+$ch = curl_init();
+$len="Content-Length: ".strlen($post);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, "Content-Type: application/x-www-form-urlencoded",$len);
+curl_setopt($ch, CURLOPT_USERAGENT, "User-Agent: okhttp/3.10.0");
+curl_setopt($ch, CURLOPT_COOKIEJAR,"$cookie");
+curl_setopt($ch, CURLOPT_COOKIEFILE,"$cookie");
+//curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
+//curl_setopt($ch, CURLOPT_PROXY, $ip);
+if($post != null){
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+}
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_TIMEOUT,5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,false);
+$exec = curl_exec($ch);
+curl_close($ch);
+return $exec;
+}
 /* setting bot */
 $ads=$t.$red."\t >> Untuk Para Jomblo Enggak Usah Nikah << ".$t.$pth."[ ".$ijo."Ada RUU KUHP Baru Perkosa Istri".$pth." ]=[".$kn."Denda 10 M".$pth."]=[ ".$tr."Colay Aja..!!!]".$t;
 
